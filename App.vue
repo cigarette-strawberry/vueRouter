3ba085ea07115f51bfd754299e31dc14f634e5ba
小吴 <2467337566@qq.com>
2023-04-23 15:32:13 +0800

    feat:vue-router学习笔记

---
+++ App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app" class="app">
+    <h1>
+      <!-- router-link 也是router提供的一个全局组件  默认渲染成a标签 tag属性可以设置渲染的标签 -->
+      <router-link tag="button" to="/">首页</router-link>
+      <router-link tag="button" to="/about">关于</router-link>
+    </h1>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: '珠峰',
+      qqqq: 6666
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ index.js
@@ -0,0 +1,83 @@
+import Vue from 'vue'
+// import VueRouter from 'vue-router'
+import VueRouter from './myrouter'
+import Home from '../views/Home.vue'
+// import Home2 from '../views/Home2.vue'
+// import Home3 from '../views/Home3.vue'
+// import Home4 from '../views/Home4.vue'
+
+// vue-router  vuex 都是vue的插件
+// Vue.use 传的参数 若是一个对象 则会找对象中的install方法执行并且会传一个Vue进去；
+//                  若是一个函数 则会直接当作 install方法执行；
+Vue.use(VueRouter)
+
+const routes = [
+  // 路由映射表
+  {
+    path: '/', // 走的路径
+    name: 'Home', // 路径对应的名字
+    component: Home, // 路径对显示的组件
+    meta: {
+      til: '首页',
+      level: 6,
+      access: 'qqq|wwww|rtrr|tetrthr'
+    },
+    redirect: '/home1', // 走进HOme组件之后 再定向到 /home1这个路径
+    children: [
+      // {
+      //   path: '/home1/:qqq/:www', // 走的路径
+      //   name: 'Home1',// 路径对应的名字
+      //   component: ()=>import('../components/home1') // 路径对显示的组件
+      // },
+      {
+        path: '/home1', // 走的路径
+        name: 'Home1', // 路径对应的名字
+        meta: {
+          til: '首页下的home1'
+        },
+        component: () => import('../components/home1') // 路径对显示的组件
+      },
+      {
+        path: '/home2', // 走的路径
+        name: 'Home2', // 路径对应的名字
+        meta: {
+          til: '首页下的home2'
+        },
+        component: () => import('../components/home2') // 路径对显示的组件
+      }
+    ]
+  },
+
+  {
+    path: '/about',
+    name: 'About',
+    meta: {
+      til: '关于',
+      level: 3
+    },
+    // route level code-splitting 路线级代码拆分
+    // this generates a separate chunk (about.[hash].js) for this route 这将为此路由生成一个单独的块
+    // which is lazy-loaded when the route is visited. 当访问该路线时，其是惰性加载的
+    component: () => import(/* webpackChunkName: "about1234" */ '../views/About.vue')
+  },
+  {
+    path: '/*',
+    redirect: '/about'
+  }
+]
+/* 
+  VueRouter 传参方式 两种  路径传参(path,query)  query传参(name,params)
+  $router 存放的是方法   $route存放是属性
+  query传参 是通过 ？的方式传递的 比较明显； 组件可以通过this.$route.query去获取对应的参数
+  params传参  需要再路由映射表的path属性处 编写  /:属性名的方式；
+              再router-link上的to 编写时 不能写path 需要写成 name
+              组件可以通过 this.$route.params获取对应的参数
+*/
+
+const router = new VueRouter({
+  mode: 'hash', // 控制的页面切换的模式  是 hash 或者 history模式； 默认 hash
+  base: process.env.BASE_URL,
+  routes //routes:routes
+})
+
+export default router
